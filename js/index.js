af2225ea65b108b18343b1d290320c87c787f643
JrTRD <ayoubtrdbuzzer@gmail.com>
2020-04-26 14:42:54 +0000

    added testimonials section

--- js/index.js
+++ js/index.js
@@ -15,3 +15,18 @@ bars.forEach((bar) => {
   const $value = progress.querySelector(".value");
   $value.textContent = `${value}%`;
 });
+
+const carouselButtons = document.querySelectorAll(
+  '[class^="carousel-button-"]'
+);
+const carouselItems = document.querySelectorAll(".carousel-item");
+
+carouselButtons.forEach((btn, i) => {
+  btn.onclick = () => {
+    console.log(btn, "clicked");
+    carouselButtons.forEach((btn) => btn.classList.remove("active"));
+    carouselItems.forEach((item) => item.classList.remove("active"));
+    btn.classList.add("active");
+    carouselItems[i].classList.add("active");
+  };
+});
