5c95e569eade59f6ce683be32dc553eac81902e8
JrTRD <ayoubtrdbuzzer@gmail.com>
2020-04-26 13:31:34 +0000

    added skills and resume section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-AOS.init();
+AOS.init({
+  duration: 800,
+});
+
+const bars = document.querySelectorAll(".bar");
+
+bars.forEach((bar) => {
+  const className = Array.from(bar.classList).find((className) =>
+    className.includes("bar-")
+  );
+  const value = parseInt(className.replace("bar-", ""));
+
+  const progress = bar.querySelector(".progress");
+  progress.style.width = `${value}%`;
+  const $value = progress.querySelector(".value");
+  $value.textContent = `${value}%`;
+});
